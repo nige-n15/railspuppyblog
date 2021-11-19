cf33236f02605183c4aea2996a58e9d21bd13375
Nigel Russell <nigel@renturapp.com>
2021-11-19 01:22:25 +0000

    bootstrap reference removed from applicatopn.scss
    not required

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_233240) do
+ActiveRecord::Schema.define(version: 2021_11_18_233359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_18_233240) do
   create_table "admins", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.string "username", null: false
+    t.string "name", null: false
   end
 
   create_table "categories", force: :cascade do |t|
